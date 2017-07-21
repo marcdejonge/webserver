69b3f47770fd84f95e80e9af3842aa3ef08b828e
Marc de Jonge <marcdejonge@gmail.com>
2017-07-21 22:49:34 +0200

    Adds application plugin for building

--- src/main/java/com/marcdejonge/webserver/HttpServer.java
+++ src/main/java/com/marcdejonge/webserver/HttpServer.java
@@ -1,5 +1,9 @@
 package com.marcdejonge.webserver;
 
 public class HttpServer {
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HttpServer.class);
 
+    public static void main(String[] args) {
+        logger.debug("Hello World!");
+    }
 }
